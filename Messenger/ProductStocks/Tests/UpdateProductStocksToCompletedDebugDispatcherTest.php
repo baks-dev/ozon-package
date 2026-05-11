820356adc85a1ed6d6120829d8d009ff7c1870ea
MichelAngelo <baksdevelopmen@gmail.com>
2026-05-11 23:03:17 +0300

    BaksDevelopment

---
+++ Messenger/ProductStocks/Tests/UpdateProductStocksToCompletedDebugDispatcherTest.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Ozon\Package\Messenger\ProductStocks\Tests;
+
+use BaksDev\Ozon\Package\Entity\Supply\OzonSupply;
+use BaksDev\Ozon\Package\Messenger\ProductStocks\UpdateProductStocksToCompletedDispatcher;
+use BaksDev\Ozon\Package\Messenger\Supply\OzonSupplyMessage;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\Attributes\Group;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\NullOutput;
+use Symfony\Component\DependencyInjection\Attribute\When;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+#[Group('ozon-package')]
+#[When(env: 'test')]
+class UpdateProductStocksToCompletedDebugDispatcherTest extends KernelTestCase
+{
+    public function testUseCase(): void
+    {
+        // Бросаем событие консольной команды
+        $dispatcher = self::getContainer()->get(EventDispatcherInterface::class);
+        $event = new ConsoleCommandEvent(new Command(), new StringInput(''), new NullOutput());
+        $dispatcher->dispatch($event, 'console.command');
+
+        /** @var UpdateProductStocksToCompletedDispatcher $UpdateProductStocksToCompletedDispatcher */
+        $CloseOzonSupplyDispatcher = self::getContainer()
+            ->get(UpdateProductStocksToCompletedDispatcher::class);
+
+        self::assertTrue(true);
+        return;
+
+        /** @var EntityManagerInterface $em */
+        $em = self::getContainer()->get(EntityManagerInterface::class);
+
+        /** @var OzonSupply|null $main */
+        $main = $em->getRepository(OzonSupply::class)
+            ->find('019cce17-70fc-75ea-82ac-37a33aa2d734');
+
+        $message = new OzonSupplyMessage(
+            $main->getId(),
+            $main->getEvent(),
+        );
+
+        $UpdateProductStocksToCompletedDispatcher($message);
+    }
+}
